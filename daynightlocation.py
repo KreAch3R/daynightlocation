a9b876f6e3ac4654f1241f9f010c2ab16d773b48
George G <kreach3r@gmail.com>
2021-09-02 12:41:37 +0300

    Second revision
    * Added dynamic geolocation by using the GPS found on the device
    * Added second dynamic geolocation by using geocoder (internet IP)
    * Added option to input lat/long manually
    
    * Added second way to get sunrise/sunset values (sunrise-sunset.org API)
    * Removed necessity to input city/location manually
    * Cleaned up the code
    * Improved printing messages

--- daynightlocation.py
+++ daynightlocation.py
@@ -1,43 +1,120 @@
 #!/usr/bin/env python3
 # Author: Justin DuBois
-# Required library - astral -
-# If missing, execute 'pip3 install astral'
+# Modified by: KreAch3R
+# Required libraries - astral, tzwhere -
+# If missing, execute 'pip3 install astral/tzwhere'
+# Also required: sudo apt-get install libgeos-dev
+# REQUIRES internet for some functions: geocoder and sunrise-sunset.org API
 
 import os
 import datetime
-from datetime import datetime
-from datetime import date
+import geocoder
+import requests
+from gps import *
+from datetime import datetime, date
 from astral import LocationInfo
 from astral.sun import sun
+from tzwhere import tzwhere
 
 inifile="/home/pi/.openauto/config/openauto_system.ini"
 
-# --------- Find today's sunise and sunset hour and minuet in city location ----------------
-city = LocationInfo("San Francisco", "Pacific", "America/Los_Angeles", 37.77397, -122.431297)
-#print((
-#    f"Information for {city.name}/{city.region}\n"
-#    f"Timezone: {city.timezone}\n"
-#    f"Latitude: {city.latitude:.02f}; Longitude: {city.longitude:.02f}\n"
-#))
+def datetime_from_utc_to_local(utc_datetime):
+    now_timestamp = time.time()
+    offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp)
+    return utc_datetime + offset
 
-s = sun(city.observer, date=date.today(), tzinfo=city.timezone)
-csrise = (s["sunrise"]).strftime('%H:%M')
-csset = (s["sunset"]).strftime('%H:%M')
+# --------- Find today's sunrise and sunset hour and minute in location ----------------
+# METHOD 1: Get longitude and latitude by right-clicking on Google Maps
+# Set this to 'True'
+manual=False
+# UNCOMMENT and input manually
+#lat=
+#long=
+if manual :
+    print ("Setting coordinates manually")
+    print ("latitude", lat)
+    print ("longitude", long)
+else :
+    # METHOD 2: Get latitude and longitude from GPS
+    # https://ozzmaker.com/using-python-with-a-gps-receiver-on-a-raspberry-pi/
+    gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
+    # 10 is a random repetition number that I've found produces valid results with a connected GPS
+    for i in range(10):
+        report = gpsd.next() #
+        if report['class'] == 'TPV':
+            lat = getattr(report,'lat',0.0)
+            long = getattr(report,'lon',0.0)
 
-#-- search in OAP system file for sunrise and sunset values --
+            print ("Getting coordinates from the connected GPS device:")
+            print ("latitude=" + str(lat))
+            print ("longitude=" + str(long))
+            break
+    else :
+        print ("GPS not found")
+        # METHOD 2: Get info from IP
+        iplocation = geocoder.ip('me')
+        print ("Getting coordinates from geocoder (device's IP):")
+        lat = iplocation.latlng[0]
+        long = iplocation.latlng[1]
+        print ("latitude", lat)
+        print ("longitude", long)
+
+# Empty line
+print ()
+
+#---------- Search for sunrise and sunset values using Astral module or sunrise-sunset.org API------------------
+
+# City names not necessary: https://stackoverflow.com/a/65433512/4008886
+# METHOD 1: Search for sunrise/sunset values using the Astral module
+# Change this to 'False' if you want to use sunrise-sunset.org
+astral=True
+
+if astral :
+    print ("Using Astral module...")
+    # Find timezone from lat/long
+    # https://stackoverflow.com/a/39457871/4008886
+    tzwhere = tzwhere.tzwhere()
+    local_tz = tzwhere.tzNameAt(lat, long) # Seville coordinates
+    print ("Timezone:", local_tz)
+
+    location = LocationInfo(timezone=local_tz, latitude=lat, longitude=long)
+    s = sun(location.observer, date=date.today(), tzinfo=location.timezone)
+
+    truncated_rise = (s["sunrise"]).strftime('%H:%M')
+    truncated_set = (s["sunset"]).strftime('%H:%M')
+else :
+    # METHOD 2: Search for sunrise/sunset values using the sunrise-sunset.org API
+    print ("Using sunrise-sunset.org API...")
+    r = requests.get('https://api.sunrise-sunset.org/json', params={'lat': float(lat), 'lng': float(long)}).json()['results']
+
+    # API uses UTC time!
+    rise_utc = datetime.strptime(r['sunrise'], '%I:%M:%S %p')
+    rise_local = datetime_from_utc_to_local(rise_utc)
+
+    set_utc = datetime.strptime(r['sunset'], '%I:%M:%S %p')
+    set_local = datetime_from_utc_to_local(set_utc)
+
+    truncated_rise = rise_local.strftime('%H:%M')
+    truncated_set = set_local.strftime('%H:%M')
+
+print ("Sunrise (local, to be installed):", truncated_rise)
+print ("Sunset (local, to be installed):", truncated_set)
+print ()
+
+#---------- Search in OAP system file for sunrise and sunset values ------------------
 with open(inifile, 'r',encoding = 'utf-8') as f:
     for line in f:
         keyword = 'SunriseTime'
         if line.startswith(keyword):
             try:
-                inisunrise = (line.split('=')[1].rstrip())
+                inirise = (line.split('=')[1].rstrip())
             except:
                 print ("Error finding SunriseTime in openauto_system.ini")
-       
+
         keyword = 'SunsetTime'
         if line.startswith(keyword):
             try:
-                inisunset = (line.split('=')[1].rstrip())
+                iniset = (line.split('=')[1].rstrip())
             except:
                 print ("Error reading SunsetTime in openauto_system.ini")
 
@@ -47,15 +124,20 @@ with open(inifile, 'r',encoding = 'utf-8') as f:
 with open(inifile, 'r',encoding = 'utf-8') as file :
   filedata = file.read()
 
-#print('inisunrise=',inisunrise)
-#print('csrise=',csrise)
-#print('inisunset=',inisunset)
-#print('csset=',csset)
+print ("Current values:")
+print ('inirise =',inirise)
+print ('iniset =',iniset)
+print ()
+print ("Changed values:")
+print ('truncated_rise =',truncated_rise)
+print ('truncated_set =',truncated_set)
+print ()
 
 # Replace the target string
-filedata = filedata.replace('SunriseTime='+inisunrise, 'SunriseTime='+csrise)
-filedata = filedata.replace('SunsetTime='+inisunset, 'SunsetTime='+csset)
+filedata = filedata.replace('SunriseTime='+inirise, 'SunriseTime='+truncated_rise)
+filedata = filedata.replace('SunsetTime='+iniset, 'SunsetTime='+truncated_set)
 
 # Write the file out again
 with open(inifile, 'w',encoding = 'utf-8') as file:
   file.write(filedata)
+  print ("inifile updated successfully")
